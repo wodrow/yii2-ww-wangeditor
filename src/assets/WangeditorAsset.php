63618557676cac15dcfe24f8515d24c12811d8db
wodrow-ys-pc <wodrow451611cv@gmail.com>
2019-02-12 12:01:08 +0800

    init

---
+++ src/assets/WangeditorAsset.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wodrow
+ * Date: 19-2-12
+ * Time: 上午11:53
+ */
+
+namespace wodrow\yii2wwwangeditor\assets;
+
+
+use yii\web\AssetBundle;
+
+class WangeditorAsset extends AssetBundle
+{
+
+}
